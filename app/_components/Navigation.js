8bab85a84dd4ce6a40c32b8d7fc8890ec5a0e089
santoshsharma27 <santosh37kr@gmail.com>
2025-01-20 23:09:10 +0530

    Added Authentication

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,13 +1,16 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
+      <ul className="flex items-center gap-16">
         <li>
           <Link
             href="/cabins"
-            className="hover:text-accent-400 transition-colors"
+            className="transition-colors hover:text-accent-400"
           >
             Cabins
           </Link>
@@ -15,18 +18,33 @@ export default function Navigation() {
         <li>
           <Link
             href="/about"
-            className="hover:text-accent-400 transition-colors"
+            className="transition-colors hover:text-accent-400"
           >
             About
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="flex items-center gap-4 transition-colors hover:text-accent-400"
+            >
+              <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="transition-colors hover:text-accent-400"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Santosh
+    <h2 className="mb-7 text-2xl font-semibold text-accent-400">
+      Welcome, {firstName}
     </h2>
   );
 }
